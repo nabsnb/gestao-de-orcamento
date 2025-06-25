df422bb138068dbec6664efa7d49691817ffe534
Nathalia <nathaliaoliveira587@gmail.com>
2025-06-24 22:37:38 -0300

    feat: validar campos, separar funções e atualizar saldo em tempo real

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const description = document.getElementById('description')
 const amount = document.getElementById('amount')
 const type = document.getElementById('type')
 const transaction = document.getElementById('transaction-list')
-const expensesValue = document.getElementById('expenses-value')
 const expensesField = document.getElementById('expenses-value')
 const incomesField = document.getElementById('incomes-value')
 const balanceField = document.getElementById('balance-value')
@@ -12,40 +11,60 @@ const incomes = []
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    validateFields()
+    if(!fieldsIsValid()) {
+        return
+    }
+    createTransaction()
+    expensesField.innerText = calculateExpenses()
+    incomesField.innerText = calculateIncomes()
+    totalBalance()
 })
+
+function errorMessage(field, message) {
+    const div = document.createElement('div')
+    div.innerText = message;
+    div.classList.add('error-message')
+    field.insertAdjacentElement('afterend', div)
+}
+function fieldsIsValid() {
+
+    let valid = true
+
+    for(let field of document.querySelectorAll('.validate')) {
+        if(!field.value){
+            errorMessage(field, `${field.ariaPlaceholder} não pode estar em branco`)
+            valid = false;
+        }
+    }
+    if(type.value === 'Selecione'){
+        errorMessage(type, 'Selecione o tipo de transação')
+    }
+    return valid
+}
 
 function createTransaction() {
     const li = document.createElement('li')
     li.innerText = `${type.value} - ${description.value} R$ ${amount.value}`
     transaction.appendChild(li)
-}
-function validateFields() {
-    if(description.value === '' || amount.value === '' || type.value === 'Selecione') {
-        console.log('Campos não podem estar vazios')
-        return
-    } else {
-        addExpenses()
-        addIncomes()
-    }
-}
 
-function addExpenses() {
-    if(type.value === 'Saida'){
+    if(type.value === 'Saida') {
         expenses.push(Number(amount.value))
-        const totalExpenses = expenses.reduce((acc, vlr) =>  acc + vlr)
-        expensesField.innerText = totalExpenses
-        createTransaction()
     }
-
-}
-
-function addIncomes() {
-    if(type.value === 'Entrada'){
+    if(type.value === 'Entrada') {
         incomes.push(Number(amount.value))
-        const totalIncomes = incomes.reduce((acc, vlr) =>  acc + vlr)
-        incomesField.innerText = totalIncomes
-        createTransaction()
     }
-
+}
+function calculateExpenses() {
+    const totalExpenses = expenses.reduce((acc, vlr) =>  acc + vlr, 0)
+    return totalExpenses
+}
+function calculateIncomes() {
+    const totalIncomes = incomes.reduce((acc, vlr) =>  acc + vlr, 0)
+    return totalIncomes
+}
+function totalBalance() {
+    const totalExpenses = calculateExpenses()
+    const totalIncomes = calculateIncomes()
+    const total = totalIncomes - totalExpenses
+    balanceField.innerText = total
 }
