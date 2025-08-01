c94b48c27ffaf6e3bd2134ab42d2fbcc16dac171
Nathalia <nathaliaoliveira587@gmail.com>
2025-07-31 21:45:42 -0300

    feat: implementar estrutura de remoção de transações via id com splice

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ const incomesField = document.getElementById('incomes-value')
 const balanceField = document.getElementById('balance-value')
 const transactions = []
 
+deleteTransaction()
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    if(!fieldsIsValid()) {
-        return
-    }
+
+    if(!fieldsIsValid()) return
+
     createTransaction()
     clearFields()
     expensesField.innerText = calculateExpenses().toFixed(2)
@@ -61,13 +63,25 @@ function getValues() {
     }
 }
 
-function createTransaction() {
-    
-    const valuesUpdated = getValues()
+function createElements() {
     const li = document.createElement('li')
-    const result = `${valuesUpdated.type} - ${valuesUpdated.description} R$ ${valuesUpdated.amount}`
+    const btn = document.createElement('button')
+    const span = document.createElement('span')
 
-    li.innerText = result
+    btn.innerText = 'x'
+    li.appendChild(span)  
+    li.appendChild(btn)
+    btn.classList.add('btn-delete')
+    return { li, span, btn }
+}
+
+function createTransaction() { 
+    const valuesUpdated = getValues()
+    const result = `${valuesUpdated.type} - ${valuesUpdated.description} R$ ${valuesUpdated.amount}`
+    const { li, span } = createElements()
+    
+    span.textContent = result
+    li.dataset.id = valuesUpdated.id
     transactionList.appendChild(li)
     transactions.push(valuesUpdated)
 }
@@ -81,16 +95,31 @@ function clearFields() {
 function calculateExpenses() {
     const transactionType = transactions.filter(transactions => transactions.type === 'Saida')
     const totalExpenses = transactionType.reduce((acc, amount) =>  acc + amount.amount, 0)
-    return totalExpenses
+    return totalExpenses;
 }
+
 function calculateIncomes() {
     const transactionType = transactions.filter(transactions => transactions.type === 'Entrada')
     const totalIncomes = transactionType.reduce((acc, amount) =>  acc + amount.amount, 0)
-    return totalIncomes
+    return totalIncomes;
 }
+
 function totalBalance() {
     const totalExpenses = calculateExpenses()
     const totalIncomes = calculateIncomes()
     const total = totalIncomes - totalExpenses
     balanceField.innerText = total.toFixed(2)
+}
+
+function deleteTransaction() {
+    transactionList.addEventListener('click', (e) => {
+        e.preventDefault()
+        if(e.target.classList.contains('btn-delete')){
+            e.target.parentElement.remove()
+            const id = e.target.parentElement.dataset.id
+            const transactionID = transactions.findIndex(idBtn => idBtn.id === Number(id))
+            transactions.splice(transactionID, 1)
+            console.log(transactions)
+        }
+    })
 }
